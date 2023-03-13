06a97037b3bd5b8603d4ab7b40593900a12b0020
Moksh Jaswal <53489593+mokshjaswal101@users.noreply.github.com>
2023-03-13 23:47:30 +0530

    legends change

--- src/data/specializations.js
+++ src/data/specializations.js
@@ -1,10 +1,12 @@
 const Specializations = {
-  "internal medicine pediatrics": "#827ad5",
   Neurology: "#63af34",
-  "Vascular Neurology": "#af2cb3",
   "Neuromuscular Medicine": "#bfd247",
-  "Hematology & Oncology": "#27360b",
-  Psychiatry: "#60b6e2",
+  "internal medicine pediatrics": "#827ad5",
+  "Clinical Neurophysiology": "#af2cb3",
+  Psychiatry: "#ADD8E6",
+  "Vascular Neurology": "#454B1B",
+  "Sleep Medicine": "#00FFFF",
+  "Neurocritical Care": "#90EE90",
   other: "#275E40",
 };
 
