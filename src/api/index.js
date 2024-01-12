c7755ebcac088f518d6e57770de31a81548d46fb
Moksh Jaswal <53489593+mokshjaswal101@users.noreply.github.com>
2024-01-12 20:55:38 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -19,8 +19,8 @@ const fetchAllData = async (projectId) => {
 
 const fetchHcpDetails = async (hcpId, projectId) => {
   try {
-    let response = await client.get(
-      `elastic/getone?uuid=${hcpId}&project_id=${projectId}`
+    let response = await axios.get(
+      `https://jbx5n3f7k0.execute-api.us-east-2.amazonaws.com/prod/getonedata?uuid=${id}&projectId=1936e211-b997-11ed-8c61-52d672bc987d`
     );
 
     return response.data;
